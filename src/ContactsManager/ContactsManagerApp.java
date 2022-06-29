9685188688d80a13784aa434088496ec78e4d766
Chris Davis <christopher.j.davis03@gmail.com>
2022-06-29 10:52:04 -0500

    basic functionality done

--- src/ContactsManager/ContactsManagerApp.java
+++ src/ContactsManager/ContactsManagerApp.java
@@ -9,6 +9,7 @@ public class ContactsManagerApp {
         switch (choice) {
             case 1:
                 ReadWrite.readContacts();
+                System.out.println("Can I assist you further?\n1: Display all contacts\n2: Add new contact\n3: Delete contact\n4: Contact search\n5: Quit");
                 int readChoice = input.nextInt();
                 choiceMade(readChoice);
                 break;
@@ -21,16 +22,28 @@ public class ContactsManagerApp {
                 String number = input.next();
                 ReadWrite.writeContact(first,last,number);
                 System.out.printf("%s, %s added to your contacts list\n", last, first);
+                System.out.println("Can I assist you further?\n1: Display all contacts\n2: Add new contact\n3: Delete contact\n4: Contact search\n5: Quit");
                 int addChoice = input.nextInt();
                 choiceMade(addChoice);
                 break;
             case 3:
-                System.out.println("Delete a contact");
+                System.out.println("Delete which contact?");
+                String deletion = input.next();
+                ReadWrite.deleteContact(deletion);
+                System.out.println("Can I assist you further?\n1: Display all contacts\n2: Add new contact\n3: Delete contact\n4: Contact search\n5: Quit");
                 int deleteChoice = input.nextInt();
                 choiceMade(deleteChoice);
                 break;
             case 4:
-                System.out.println("Search for a contact");
+                System.out.println("Enter search term");
+                String search = input.next();
+                Contact found = ReadWrite.searchContact(search);
+                if (found == null){
+                    System.out.println("Contact not found");
+                } else {
+                    System.out.printf("Name: %s, Number %s\n", found.getName(), found.getNumber());
+                }
+                System.out.println("Can I assist you further?\n1: Display all contacts\n2: Add new contact\n3: Delete contact\n4: Contact search\n5: Quit");
                 int searchChoice = input.nextInt();
                 choiceMade(searchChoice);
                 break;
@@ -38,14 +51,17 @@ public class ContactsManagerApp {
                 System.out.println("Are you sure you want to quit?\n(yes/no)");
                 String quit = input.next();
                 if (quit.equalsIgnoreCase("yes")){
+                    ReadWrite.cleanEnd();
                     break;
                 } else {
                     System.out.println("Input not recognized. Please select a valid option.");
+                    System.out.println("Can I assist you further?\n1: Display all contacts\n2: Add new contact\n3: Delete contact\n4: Contact search\n5: Quit");
                     int newChoice = input.nextInt();
                     choiceMade(newChoice);
                 }
             default:
                 System.out.println("Input not recognized. Please select a valid option.");
+                System.out.println("Can I assist you further?\n1: Display all contacts\n2: Add new contact\n3: Delete contact\n4: Contact search\n5: Quit");
                 int newChoice = input.nextInt();
                 choiceMade(newChoice);
         }
@@ -53,7 +69,7 @@ public class ContactsManagerApp {
 
     public static void main(String[] args) {
         ReadWrite.createContacts();
-        System.out.println("Welcome to Contacts Manager, how can I assist you?");
+        System.out.println("Welcome to Contacts Manager, how can I assist you?\n1: Display all contacts\n2: Add new contact\n3: Delete contact\n4: Contact search\n5: Quit");
         int choice = input.nextInt();
         choiceMade(choice);
     }
